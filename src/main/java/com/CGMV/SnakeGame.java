5d0851a584df92d1aec7cdbce080008a0510129c
Caio RIbeiro <caioalvarenga.ribeiro@estudante.ufjf.br>
2024-09-05 18:23:42 -0300

    Sugere substituir a função cria por um construtor e adiciona atributos

--- src/main/java/com/CGMV/SnakeGame.java
+++ src/main/java/com/CGMV/SnakeGame.java
@@ -1,16 +1,17 @@
 package com.CGMV;
 
 
-import CGMV.view.TelaInicial;
+import com.CGMV.view.TelaInicial;
+
+import java.util.Random;
 
 public class SnakeGame {
 
 
     public static void main(String[] args)
     {
-
         TelaInicial teste = new TelaInicial();
-        teste.draw();
+        //teste.draw();
 
     }
 }
--- src/main/java/com/CGMV/view/TelaInicial.java
+++ src/main/java/com/CGMV/view/TelaInicial.java
@@ -1,4 +1,4 @@
-package CGMV.view;
+package com.CGMV.view;
 
 import javax.swing.*;
 import java.awt.*;
@@ -6,19 +6,25 @@ import java.awt.*;
 public class TelaInicial  {
 
     private JFrame window;
-    private final int WD = 800;
-    private final int HG = 600;
+    private final int LAR = 800;
+    private final int ALT = 600;
+    private final String FONTE = " "; // definir fonte
+    private final int []EIXO_X = new int [10]; // ver quantidade de unidades
+    private final int []EIXO_Y = new int [10]; // ver quantidade de unidades
 
+    public TelaInicial() {
+        window = new JFrame("Inicio");
+        window.setSize(LAR, ALT);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
 
-
-    public void draw(){
+    /*public void draw(){
     window = new JFrame("Inicio");
     window.setSize(WD, HG);
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     window.setVisible(true);
-
-
-
     }
+    */
 
 }
