c88f050f727383bc613d474a2952dccd67ec83ed
Caio RIbeiro <caioalvarenga.ribeiro@estudante.ufjf.br>
2024-09-05 17:47:23 -0300

    Cria atributos e construtor da classe cobra

--- src/main/java/com/CGMV/Entities/Cobra.java
+++ src/main/java/com/CGMV/Entities/Cobra.java
@@ -1,4 +1,16 @@
 package com.CGMV.Entities;
 
 public class Cobra {
+    private final int SPEED = 200;
+    private int tamCobra;
+    private int frutasComidas;
+    private char direcao;
+    private boolean estaViva;
+
+    public Cobra() {
+        tamCobra = 4;
+        frutasComidas = 0;
+        direcao = 'd';
+        estaViva = true;
+    }
 }
