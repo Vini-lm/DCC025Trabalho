1e6f9da34c090797f86fd3d2c522c3877346e3a8
Vinicius <133075570+Vini-lm@users.noreply.github.com.>
2024-09-08 05:34:18 -0300

    Adicionando jogo na tela

--- src/main/java/com/CGMV/Jogo/Game.java
+++ src/main/java/com/CGMV/Jogo/Game.java
@@ -1,7 +1,26 @@
 package com.CGMV.Jogo;
 
-public class Game {
+import com.CGMV.view.TelaJogo;
+
+import javax.swing.*;
+
+public class Game extends JFrame {
 
 
-    private boolean isRodando = false;
+    public static void main() { new Game();}
+
+
+    public Game()
+    {
+        add(new TelaJogo()); // adiciona o painel da tela do jogo na tela
+        setTitle("Jogo");
+        setResizable(false);
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+    }
+
+
 }
