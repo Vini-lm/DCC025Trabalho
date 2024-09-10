97ba0bde02a7cf136b64704ba6797baee09fbf21
marco <marcosvinicius.ferreira@estudante.ufjf.br>
2024-09-10 11:42:22 -0300

    Alterou a lógica da janela

--- src/main/java/com/CGMV/view/TelaInicial.java
+++ src/main/java/com/CGMV/view/TelaInicial.java
@@ -1,29 +1,39 @@
 package com.CGMV.view;
 
+import com.CGMV.Controler.IniciarJogo;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 
-public class TelaInicial  {
+public class TelaInicial extends JFrame implements ActionListener {
 
-    private JFrame window;
+   // private JFrame window;
     private final int LAR = 800;
     private final int ALT = 600;
     private JPanel painel;
     private JButton button;
 
     public TelaInicial() {
-        window = new JFrame("Inicio");
-        window.setSize(LAR, ALT);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //window = new JFrame("Inicio");
+        //window.setSize(LAR, ALT);
+        //window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(LAR, ALT);
         button = new JButton("Iniciar jogo");
         painel = new JPanel();
         painel.setLayout(null);
         button.setBounds(325 ,275,100,50);
         button.setToolTipText("Clique para iniciar jogo");
         painel.add(button);
-        window.add(painel);
-        window.setVisible(true);
+        button.addActionListener(new IniciarJogo(button,this));
+        add(painel);
+        setVisible(true);
+       // window.add(painel);
+       // window.setVisible(true);
     }
+
 
     /*public void draw(){
     window = new JFrame("Inicio");
@@ -33,4 +43,8 @@ public class TelaInicial  {
     }
     */
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
