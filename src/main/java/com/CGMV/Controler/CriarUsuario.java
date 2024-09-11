6d441db783bb0cf16b965460fd2d4359011f2fb8
Caio RIbeiro <caioalvarenga.ribeiro@estudante.ufjf.br>
2024-09-10 22:51:08 -0300

    Define botão "criar usuário"

---
+++ src/main/java/com/CGMV/Controler/CriarUsuario.java
@@ -0,0 +1,26 @@
+package com.CGMV.Controler;
+
+import com.CGMV.view.TelaEdicao;
+import com.CGMV.view.TelaInicial;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+
+public class CriarUsuario extends MouseAdapter implements ActionListener {
+    private final JButton button;
+    private final JFrame frame;
+
+    public CriarUsuario(JButton button, JFrame frame) {
+        this.button = button;
+        this.frame = frame;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        TelaEdicao nova = new TelaEdicao();
+        frame.setVisible(false);
+    }
+}
