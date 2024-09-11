5c403ec106d38d3ddf282142d61e1b05f784f523
Caio RIbeiro <caioalvarenga.ribeiro@estudante.ufjf.br>
2024-09-10 22:51:36 -0300

    Define botão "usuario padrão"

---
+++ src/main/java/com/CGMV/Controler/PadronizarUsuario.java
@@ -0,0 +1,26 @@
+package com.CGMV.Controler;
+
+import com.CGMV.Jogo.Game;
+import com.CGMV.view.TelaInicial;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+
+public class PadronizarUsuario extends MouseAdapter implements ActionListener {
+    private final JButton button;
+    private final JFrame frame;
+
+    public PadronizarUsuario(JButton button, JFrame frame) {
+        this.button = button;
+        this.frame = frame;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        TelaInicial nova = new TelaInicial();
+        frame.setVisible(false);
+    }
+}
