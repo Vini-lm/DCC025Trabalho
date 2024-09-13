6e6be98bb3563514bac467717ba8e90785e5bae3
Vinicius <133075570+Vini-lm@users.noreply.github.com.>
2024-09-13 02:36:45 -0300

    Tamanho das unidades corrigido, criação de métodos e alternância entre frutas

--- src/main/java/com/CGMV/view/TelaJogo.java
+++ src/main/java/com/CGMV/view/TelaJogo.java
@@ -1,8 +1,10 @@
 package com.CGMV.view;
 
 import com.CGMV.Controler.GameKeyListener;
+import com.CGMV.Entities.Banana;
 import com.CGMV.Entities.Cobra;
 import com.CGMV.Entities.Fruta;
+import com.CGMV.Entities.Uva;
 import com.CGMV.Jogo.Game;
 
 import javax.swing.*;
@@ -19,7 +21,7 @@ public class TelaJogo extends JPanel implements ActionListener {
     private Cobra cobra;
     private Fruta fruta;
     private int pTabuleiro = 25;
-    private int unidades = (LAR*ALT)/pTabuleiro*pTabuleiro;
+    private int unidades = (LAR*ALT)/(pTabuleiro*pTabuleiro);
     private final String FONTE = " "; // definir fonte
     private final int []EIXO_X = new int [unidades]; // ver quantidade de unidades
     private final int []EIXO_Y = new int [unidades]; // ver quantidade de unidades
@@ -32,7 +34,7 @@ public class TelaJogo extends JPanel implements ActionListener {
     public TelaJogo()
     {
         cobra = new Cobra();
-        fruta = new Fruta("banana");
+        fruta = new Uva();
         random = new Random();
         delay = cobra.getSPEED();
         setFocusable(true);
@@ -77,23 +79,13 @@ public class TelaJogo extends JPanel implements ActionListener {
     {
         if(isRunning)
         {
-            for(int i = 0; i < LAR;i+=pTabuleiro)
-                for(int j = 0; j < ALT;j+=pTabuleiro)
-                {
-                    if((i + j) %2 == 0) {
-                        g.setColor(new Color(142, 204, 59));
-                        g.fillRect(i, j, pTabuleiro, pTabuleiro);
-                    }
-                     else
-                    {
-                        g.setColor(new Color(167, 218, 74));
-                        g.fillRect(i, j,pTabuleiro,pTabuleiro);
-                    }
-                }
 
+            desenhaFundo(g);
 
-            g.setColor(Color.red);
-            g.setColor(Color.red); // cor da fruta
+            //g.drawImage(new ImageIcon(fruta.getTexture()).getImage(),fruta.getIdX(), fruta.getIdY(),pTabuleiro,pTabuleiro, this);
+
+                g.setColor(fruta.getColor());
+
             g.fillOval(fruta.getIdX(),fruta.getIdY(),pTabuleiro,pTabuleiro); // desenha a fruta na posição dela com o tamanho proporcional ao tabuleiro
 
             for(int i = 0; i < cobra.getTamCobra(); i++) {
@@ -101,14 +93,20 @@ public class TelaJogo extends JPanel implements ActionListener {
                     g.setColor(new Color(33, 5, 104));
                     g.fillRect(EIXO_X[0], EIXO_Y[0], pTabuleiro, pTabuleiro);
                 } else if(i % 2 == 0) { // desenha o corpo dela
-                g.setColor(new Color(77, 120, 247));
-                g.fillRect(EIXO_X[i], EIXO_Y[i], pTabuleiro, pTabuleiro);
-                }
-                else {
+                    g.setColor(new Color(77, 120, 247));
+                    g.fillRect(EIXO_X[i], EIXO_Y[i], pTabuleiro, pTabuleiro);
+                } else { // desenha o corpo dela
                     g.setColor(new Color(77, 120, 247));
                     g.fillRect(EIXO_X[i], EIXO_Y[i], pTabuleiro, pTabuleiro);
                 }
             }
+
+
+       // for(int i = cobra.getTamCobra() + 5; i >= cobra.getTamCobra(); i--) {
+         //   g.setColor(new Color(23, 142, 25));
+           // g.fillRect(EIXO_X[i], EIXO_Y[i], pTabuleiro, pTabuleiro);
+        //}
+
 
       /*  for(int i =0; i<ALT; i++)
         {
@@ -127,19 +125,36 @@ public class TelaJogo extends JPanel implements ActionListener {
         }
 */
 
-
         }
         else
         {
-            TelaFimDeJogo fim = new TelaFimDeJogo();
+            new TelaFimDeJogo();
             timer.stop();
         }
     }
 
 
+    private void desenhaFundo(Graphics g)
+    {
+        for(int i = 0; i < LAR;i+=pTabuleiro)
+            for(int j = 0; j < ALT;j+=pTabuleiro)
+            {
+                if((i + j) %2 == 0) {
+                    g.setColor(new Color(142, 204, 59));
+                    g.fillRect(i, j, pTabuleiro, pTabuleiro);
+                }
+                else
+                {
+                    g.setColor(new Color(167, 218, 74));
+                    g.fillRect(i, j,pTabuleiro,pTabuleiro);
+                }
+            }
+    }
+
+
     private void walk()
     {
-        for(int i = cobra.getTamCobra(); i > 0; i--) // move o corpo da cobra pra frente
+        for(int i = cobra.getTamCobra() + fruta.getPoints() ; i > 0; i--) // move o corpo da cobra pra frente
         {
             EIXO_X[i] = EIXO_X[i - 1];
             EIXO_Y[i] = EIXO_Y[i - 1];
@@ -151,8 +166,8 @@ public class TelaJogo extends JPanel implements ActionListener {
                 break;
 
             case'D':
-            EIXO_X[0] = EIXO_X[0] + pTabuleiro; // move a cobra para a direita
-            break;
+                EIXO_X[0] = EIXO_X[0] + pTabuleiro; // move a cobra para a direita
+                break;
 
             case'E':
                 EIXO_X[0] = EIXO_X[0] - pTabuleiro; // move a cobra para a esquerda
@@ -168,15 +183,30 @@ public class TelaJogo extends JPanel implements ActionListener {
 
     private void checkColisao()
     {
+
+        if(EIXO_X[0] == fruta.getIdX() && EIXO_Y[0] == fruta.getIdY()) // se a cobra encostar na comida
+        {
+            cobra.feed(fruta); // aumenta o tamanho dela
+
+            if(fruta instanceof Uva) // se a fruta for uva
+                fruta = new Banana(); // faz a fruta ser uma banana
+            else if(fruta instanceof Banana)// se a fruta for uma banana
+                fruta = new Uva(); // faz fruta ser uma uva
+
+
+            setFrutaPos(); // gera uma nova posição para a comida
+
+
+
+        }
+
+
         for(int i = cobra.getTamCobra(); i > 0; i--)
         {
-            if(EIXO_X[i] == fruta.getIdX() && EIXO_Y[i] == fruta.getIdY()) // se a cobra encostar na comida
-            {
-                cobra.feed(); // aumenta o tamanho dela
-                setFrutaPos(); // gera uma nova posição para a comida
-            }
             if(EIXO_X[0] == EIXO_X[i] && EIXO_Y[0] == EIXO_Y[i]) // verifica se a cobra bateu no próprio corpo
                 isRunning = false;
+
+
         }
         if(EIXO_X[0] < 0  || EIXO_X[0] >= LAR) // Se a cobra ultrapassar os limites da tela(bater em um dos lados)
         {
