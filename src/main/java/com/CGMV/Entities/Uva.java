e7a8256fe23db90163ede00491b481bcffe61a12
Vinicius <133075570+Vini-lm@users.noreply.github.com.>
2024-09-13 02:31:30 -0300

    Adiciona a classe Uva

---
+++ src/main/java/com/CGMV/Entities/Uva.java
@@ -0,0 +1,27 @@
+package com.CGMV.Entities;
+
+
+import java.awt.*;
+
+public class Uva extends Fruta{
+
+
+    // private final Image skin = new Image;
+
+
+
+   public Uva()
+   {
+       this.texture = "";
+       this.cor = new Color(152, 5, 205);
+   }
+
+    @Override
+    public int getPoints() {
+        return 2;
+    }
+
+
+
+
+}
