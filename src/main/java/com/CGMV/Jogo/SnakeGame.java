2c00ab5b9198669c8c789729e3037afc50906613
Vinicius <133075570+Vini-lm@users.noreply.github.com.>
2024-09-29 08:03:53 -0300

    Teste

---
+++ src/main/java/com/CGMV/Jogo/SnakeGame.java
@@ -0,0 +1,31 @@
+package com.CGMV.Jogo;
+
+
+import com.CGMV.persistence.Config;
+import com.CGMV.persistence.profile.Adm;
+import com.CGMV.view.frame.MainScreen;
+
+
+public class SnakeGame {
+
+
+    public static void main(String[] args)
+    {
+        //TelaInicial teste = new TelaInicial();
+        //TelaJogo teste2 = new TelaJogo();
+        //TelaUsuario teste3 = new TelaUsuario();
+        //teste.draw();
+        //Game game = new Game();
+
+        Config teste = new Config(false,true,false,false,null,null);
+        Adm teste1 = new Adm("teste", teste);
+
+        teste1.init();
+
+
+
+        MainScreen mainScreen = new MainScreen(teste1);
+
+
+    }
+}
