184c9f2d30bcb94d3526c467e22fe89963a68510
Vinicius <133075570+Vini-lm@users.noreply.github.com.>
2024-09-29 07:36:42 -0300

    Criação da fruta maca

---
+++ src/main/java/com/CGMV/Entities/Maca.java
@@ -0,0 +1,15 @@
+package com.CGMV.Entities;
+
+import java.awt.*;
+
+public class Maca  extends Fruta{
+
+
+    public Maca()
+    {
+        this.texture = "";
+        this.cor = new Color(221, 5, 45);
+    }
+
+
+}
