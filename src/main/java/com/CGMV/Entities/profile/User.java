a79ace20904d422cebe01e2974e81b0f20a81642
Guilherme <perisseguilhermeg@gmail.com>
2024-10-01 23:29:47 -0300

    Adiciona atributos de cor

--- src/main/java/com/CGMV/Entities/profile/User.java
+++ src/main/java/com/CGMV/Entities/profile/User.java
@@ -4,6 +4,7 @@ import com.CGMV.Entities.Cobra;
 import com.CGMV.persistence.Config;
 import com.CGMV.persistence.Data;
 
+import java.awt.*;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -13,10 +14,12 @@ public abstract class User {
     protected String name;
     public Data data;
     protected Config config;
+    private Color cor1;
+    private Color cor2;
 
 
     public User(String name) {this.name = name;
-    this.data = new Data(null);}
+        this.data = new Data(null);}
 
 
     public String getName(){ return this.name;}
@@ -44,6 +47,20 @@ public abstract class User {
     public void save(Config config){data.saveConfig(config,this);}
 
 
+    public void setCor1(Color cor1) {
+        this.cor1 = cor1;
+    }
 
+    public void setCor2(Color cor2) {
+        this.cor2 = cor2;
+    }
+
+    public Color getCor1() {
+        return cor1;
+    }
+
+    public Color getCor2() {
+        return cor2;
+    }
     public void setNome(String nome){this.name = nome;}
 }
